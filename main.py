b44e5cd2af46336f592d5a7ac7199513a7cafb39
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-01 20:08:25 +0100

    Add time.sleep()

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from turtle import Turtle, Screen
+import time
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
+segments = []
 
 for position in starting_positions:
     new_segment = Turtle(shape="square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
+
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.5)
+    for seg in segments:
+        seg.forward(20)
 
 
 screen.exitonclick()
