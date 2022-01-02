fcdd42beebdd2f2429716bfa7d2677882e01a998
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-02 09:49:03 +0100

    Add onkey functions

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from turtle import Turtle, Screen
 import time
 from Snake import Snake
 
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -11,6 +10,11 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 is_game_on = True
 while is_game_on:
