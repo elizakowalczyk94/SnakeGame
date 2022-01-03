32dd4f82960a23cc1baa7d5aa0cb69f407fbc46a
Eliza Kowalczyk <elizakowalczyk94@gmail.com>
2022-01-03 21:03:19 +0100

    Detect collision with food - if statement

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from turtle import Turtle, Screen
 import time
 from Snake import Snake
+import food
+
+food = food.Food()
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -21,5 +24,10 @@ while is_game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
 
 screen.exitonclick()
